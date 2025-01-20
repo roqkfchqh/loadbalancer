0239a3cf3a46f6f1bddb75ebcacb639a9ebddec8
roqkfchqh <plmokn48@daum.net>
2025-01-21 01:26:02 +0900

    feat: atomicInteger 사용

--- src/main/java/com/example/loadbalancer/core/LoadBalancer.java
+++ src/main/java/com/example/loadbalancer/core/LoadBalancer.java
@@ -1,6 +1,7 @@
 package com.example.loadbalancer.core;
 
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.atomic.AtomicInteger;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -11,29 +12,13 @@ import java.util.List;
 @Slf4j
 @Component
 public class LoadBalancer {
+    //스프링부트 자체가 api 처리 시에 비동기로 처리되는데 왜 synchronized 씀?
 
     private final HealthCheckService healthCheckService;
-    private int currentIndex = 0;
+    private final AtomicInteger currentIndex = new AtomicInteger(0);
 
     public LoadBalancer(HealthCheckService healthCheckService) {
         this.healthCheckService = healthCheckService;
-    }
-
-    //현재 정상서버 리스트 가져옴
-    //동기화 블록 사용해 멀티스레드 환경에서의 동시성 문제 방지
-    public synchronized String getNextServer() {
-        List<String> healthyServers = healthCheckService.getHealthyServers();
-
-        //정상서버가 하나도 없는경우
-        if (healthyServers.isEmpty()) {
-            throw new IllegalStateException("No healthy servers available.");
-        }
-
-        //라운드로빈 방식으로 서버 순회 -> 다음 서버 선택
-        String server = healthyServers.get(currentIndex);
-        //인덱스는 리스트 크기를 초과하지 않도록 순환됨
-        currentIndex = (currentIndex + 1) % healthyServers.size();
-        return server;
     }
 
     //다음 정상서버를 가져오고 클라이언트 요청 전달 준비
@@ -58,5 +43,21 @@ public class LoadBalancer {
             log.info("Request forwarded to {} with response: {}", server, response);
             return response.toString();
         }
+    }
+
+    //현재 정상서버 리스트 가져옴
+    //동기화 블록 사용해 멀티스레드 환경에서의 동시성 문제 방지
+    private String getNextServer() {
+        List<String> healthyServers = healthCheckService.getHealthyServers();
+
+        //정상서버가 하나도 없는경우
+        if (healthyServers.isEmpty()) {
+            throw new IllegalStateException("No healthy servers available.");
+        }
+
+        //라운드로빈 방식으로 서버 순회 -> 다음 서버 선택
+        //인덱스는 리스트 크기를 초과하지 않도록 순환됨
+        int index = currentIndex.getAndUpdate(i -> (i + 1) % healthyServers.size());
+        return healthyServers.get(index);
     }
 }
